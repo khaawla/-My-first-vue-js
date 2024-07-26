9266149dfa2dc9a472706b59d8d4ce1863806a33
khaawla <khaoulaelmissaoui@gmai.com>
2024-07-26 10:15:45 +0100

    save

--- main.js
+++ main.js
@@ -2,25 +2,27 @@ const app=Vue.createApp({
     data() {
        return {
           cart: 0 ,
+          onSale: true ,
           product: 'socks',
           brand: 'vue Mastery',
-          image:'./assets/images/socks_green.jpg',
+          selectedVariant:0,
           inventory: 0 ,
           details: ['50% cotton','30% wool', '20% polyester'],
           variants: [
-            { id: 2234 , color: 'green' , image:'./assets/images/socks_green.jpg', quantity:50 },
-            { id:2235 , color: '#0c0b54', image: './assets/images/socks_blue.jpg' , quantity:0},
+            { id: 2234 , color: 'green' , image:'./assets/images/socks_green.jpg', quantity:0 },
+            { id:2235 , color: '#0c0b54', image: './assets/images/socks_blue.jpg' , quantity:50},
           ],
           sizes: ['S', 'M', 'L', 'XL'],
-          instock: true
+        
     }
 },
 methods: {
   addtocart() {
     this.cart +=1
   },
-  updateImage(variantImage){
-    this.image=variantImage
+  updateVariant(index){
+    this.selectedVariant=index
+    console.log(index)
   },
   decrementcart(){
     if(this.cart > 0){
@@ -31,6 +33,22 @@ methods: {
 computed:{
   title() {
     return this.brand + ' ' +this.product
+  },
+  image() {
+    return this.variants[this.selectedVariant].image
+  },
+  instock() {
+    return this.variants[this.selectedVariant].quantity
+  },
+  sale(){
+    if(this.onSale) {
+      return this.brand+' '+this.product+' ,is on sale.';
+    } else { 
+      return this.brand + ' ' +this.product;
+     
+    
   }
+  }
+
 }
 })
